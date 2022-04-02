6860e94c31b945517d108f313844e61d9be7210d
KareemHakem <km01286486191@gmail.com>
2022-04-02 03:02:33 +0200

    Fix Style

--- src/components/userComponints/UserCreateReportInput/index.js
+++ src/components/userComponints/UserCreateReportInput/index.js
@@ -3,6 +3,7 @@ import { Formik, Form } from "formik";
 import { FormInput } from "../../FormInput";
 import { validationSchemaUserReportInfluencer as validationSchema } from "../../../utils/validationSchema";
 import { Button } from "../../../commons/Button";
+
 import "./style.css";
 
 export default function UserCreateReportInput({
@@ -20,30 +21,33 @@ export default function UserCreateReportInput({
         onSubmit={handleUserReportInfluencer}
       >
         {({ isValid, dirty }) => (
-          <Form>
-            <FormInput
-              name="infName"
-              label="Influencer Name"
-              placeholder="Influencer Name"
-              width={660}
-            />
-            <FormInput
-              name="description"
-              label="Description"
-              placeholder="Description"
-              width={660}
-              variant="outlined"
-              multiline
-              rows={3}
-            />
-            <Button
-              className="influencer_edit_form_btn"
-              title="create"
-              type="submit"
-              disabled={!dirty || !isValid}
-              loading={loading}
-              width={100}
-            />
+          <Form className="userInputReportCardContainer">
+            <h1>Here you can report influencer</h1>
+            <div className="userInputReportCard">
+              <FormInput
+                name="infName"
+                label="Influencer Name"
+                placeholder="Influencer Name"
+                width={660}
+              />
+              <FormInput
+                name="description"
+                label="Description"
+                placeholder="Description"
+                width={660}
+                variant="outlined"
+                multiline
+                rows={3}
+              />
+              <Button
+                className="influencer_edit_form_btn"
+                title="REPORT"
+                type="submit"
+                disabled={!dirty || !isValid}
+                loading={loading}
+                width={100}
+              />
+            </div>
           </Form>
         )}
       </Formik>

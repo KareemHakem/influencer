69cc58d65819c70c34466fd457fd82adbc39f53c
KareemHakem <km01286486191@gmail.com>
2022-05-01 09:18:13 +0200

    add animation home page style

--- src/components/HomeComponents/HomeAnimation/index.js
+++ src/components/HomeComponents/HomeAnimation/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Lottie from "react-lottie";
 import animation from "../../../assets/json/homeAnimation.json";
+import animation2 from "../../../assets/json/arrowAnimation.json";
+import animation3 from "../../../assets/json/influencerAnimation.json";
+import animation4 from "../../../assets/json/shadowTextAnimation.json";
 import { Button } from "../../../commons/Button";
 import "./style.css";
 
@@ -12,6 +15,45 @@ export const HomeAnimation = () => {
   };
   return (
     <div className="homePageAnimation">
+      <Lottie options={defaultOptions} />
+    </div>
+  );
+};
+
+export const HomeArrowAnimation = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation2,
+  };
+  return (
+    <div className="homePageAnimation">
+      <Lottie options={defaultOptions} />
+    </div>
+  );
+};
+
+export const InfluencerAnimation = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation3,
+  };
+  return (
+    <div className="homePageAnimation">
+      <Lottie options={defaultOptions} />
+    </div>
+  );
+};
+
+export const ShadowTextAnimation = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation4,
+  };
+  return (
+    <div>
       <Lottie options={defaultOptions} />
     </div>
   );
--- src/pages/main/HomePage/index.js
+++ src/pages/main/HomePage/index.js
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { HomeAnimation } from "../../../components/HomeComponents/HomeAnimation";
+import { HomeArrowAnimation } from "../../../components/HomeComponents/HomeAnimation";
+import { InfluencerAnimation } from "../../../components/HomeComponents/HomeAnimation";
+import { ShadowTextAnimation } from "../../../components/HomeComponents/HomeAnimation";
+
 import HomeText from "../../../components/HomeComponents/HomeText";
 
 import "./style.css";
@@ -19,13 +23,33 @@ export default function HomePage() {
   };
 
   return (
-    <div className="home-page-Animation">
-      <div className="home-page-Animation_container">
-        <HomeText handleUserInfluencerNavigate={handleUserInfluencerNavigate} />
-        <HomeAnimation />
+    <div className="homePage">
+      <div className="home-page-Animation">
+        <div className="home-page-Animation_container">
+          <HomeText
+            handleUserInfluencerNavigate={handleUserInfluencerNavigate}
+          />
+          <HomeAnimation />
+        </div>
+        <div className="home-page-animation-arrow">
+          <HomeArrowAnimation />
+        </div>
+        <div className="home-page-Animation-info_part">
+          <InfluencerAnimation />
+          <div className="animation">
+            <ShadowTextAnimation />
+            <div className="descriptionTextAnimation">
+              <h1> Connect with all Influencer </h1>
+              <p>
+                In any easy way, you can communicate with all famous people on
+                social media to order everything you wish for with easy and safe
+                payment methods
+              </p>
+            </div>
+          </div>
+        </div>
+        <div style={{ height: 50 }}></div>
       </div>
     </div>
   );
 }
-
-
